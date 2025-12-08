61d0be03c55c4e0d3efb4fdb36f5b06d4633b6ec
EstebanUnal-Hub <mpenach@unal.edu.co>
2025-12-07 20:22:54 -0500

    it was changed femto folder

--- TT_FemtoRV/FemtoRV_UN-main/src/project.v
+++ TT_FemtoRV/FemtoRV_UN-main/src/project.v
@@ -18,30 +18,31 @@ module tt_um_femto (
 
   // All output pins must be assigned. If not used, assign to 0.
   
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out = 8'h00;
+	assign uio_oe = 8'h00;
 
   /* verilator lint_off SYNCASYNCNET */
   reg resetn;
   /* verilator lint_on SYNCASYNCNET */
   always @(negedge clk) resetn <= rst_n;
 
-  femto femto0(
-    .clk(clk),
-    .resetn(resetn),
-    .spi_mosi(uo_out[0]),
-    .spi_mosi_ram(uo_out[1]),
-    .spi_cs_n(uo_out[2]),
-    .spi_cs_n_ram(uo_out[3]),
-    .spi_clk_ram(uo_out[4]),
-    .spi_clk(uo_out[5]),
-    .LEDS(uo_out[6]),
-    .TXD(uo_out[7]),
-
-    .spi_miso(ui_in[0]),
-    .spi_miso_ram(ui_in[1]),
-    .RXD(ui_in[2])
-  );
+  femto femto0( 
+		.clk          (clk), 
+		.resetn       (resetn), 
+		.spi_mosi     (uo_out[0]),
+		.spi_mosi_ram (uo_out[1]),
+		.spi_cs_n     (uo_out[2]),
+		.spi_cs_n_ram (uo_out[3]),
+		.spi_clk_ram  (uo_out[4]),		
+		.spi_clk      (uo_out[5]),
+		.LEDS         (uo_out[6]),
+		.TXD          (uo_out[7]),
+		
+		.spi_miso     (ui_in[0]),
+		.spi_miso_ram (ui_in[1]),
+		.RXD          (ui_in[2])
+		
+	);
 
 
   // List all unused inputs to prevent warnings
